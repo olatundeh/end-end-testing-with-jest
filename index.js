73974f3d874b343dcfed82892bf66879c5145e28
Olatunde Haroun <haroun.ola@gmail.com>
2024-01-10 15:32:09 +0000

    first commit

--- index.js
+++ index.js
@@ -4,25 +4,25 @@ const cors = require('cors');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
 
-const app = express();
+const initializeApp = () => {
 
-// add other middleware
-app.use(cors());
-app.use(morgan('dev'));
+  const app = express();
 
-// parse application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: false }));
+  // add other middleware
+  app.use(cors());
+  app.use(morgan('dev'));
 
-// parse application/json
-app.use(express.json());
+  // parse application/x-www-form-urlencoded
+  app.use(express.urlencoded({ extended: false }));
 
-app.use('/', routes);
+  // parse application/json
+  app.use(express.json());
 
-const config = {
-  port: process.env.PORT || 8080,
-};
+  app.use('/', routes);
 
-app.listen(config.port, () => {
-  // eslint-disable-next-line no-console
-  console.log('Express server listening on port', config.port);
-});
+  return app;
+
+}
+
+const app = initializeApp();
+module.exports = app;
